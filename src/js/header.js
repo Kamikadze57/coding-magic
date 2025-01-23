ff98c58ae892eb23d3972733383c677d83912d1b
varvaramois <143871594+varvaramois@users.noreply.github.com>
2025-01-23 20:42:03 +0100

    started to make light-dark theme

--- src/js/header.js
+++ src/js/header.js
@@ -5,8 +5,31 @@ dropbtn.addEventListener('click', () => {
   dropdownContent.style.display = 'block';
 });
 
-document.addEventListener('click', (event) => {
-  if (!event.target.matches('[drop-content]') && !event.target.matches('[dropbtn]')) {
+document.addEventListener('click', event => {
+  if (
+    !event.target.matches('[drop-content]') &&
+    !event.target.matches('[dropbtn]')
+  ) {
     dropdownContent.style.display = 'none';
   }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.getElementById('theme-toggle');
+  const body = document.body;
+
+  if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-theme');
+    themeToggle.checked = true;
+  }
+
+  themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+      body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
+  });
 });
