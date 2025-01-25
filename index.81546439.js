7c3d24d57973fdb1d9b55856d9ec5c0076824db9
Kamikadze57 <143871458+Kamikadze57@users.noreply.github.com>
2025-01-25 13:04:41 +0000

    Deploying to gh-pages from @ Kamikadze57/coding-magic@aca39a76a7aba93df0763d5b07e957899329c619 🚀

--- index.81546439.js
+++
@@ -1,2 +0,0 @@
-!function(){let e=document.querySelector("[dropbtn]"),t=document.querySelector("[drop-content]");e.addEventListener("click",()=>{t.style.display="block"}),document.addEventListener("click",e=>{e.target.matches("[drop-content]")||e.target.matches("[dropbtn]")||(t.style.display="none")})}();
-//# sourceMappingURL=index.81546439.js.map
--- index.acbcd86d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[dropbtn]"),t=document.querySelector("[drop-content]");e.addEventListener("click",()=>{t.style.display="block"}),document.addEventListener("click",e=>{e.target.matches("[drop-content]")||e.target.matches("[dropbtn]")||(t.style.display="none")});
-//# sourceMappingURL=index.acbcd86d.js.map
